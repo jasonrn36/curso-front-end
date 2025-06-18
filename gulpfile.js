c41ea4d4d5caf5b7e1d490cd84233ffed54157ec
Jason Ramalho Nascimento <jason.rn36@gmail.com>
2025-06-18 11:58:44 -0400

    inserindo o outputStyle: 'compressed' no gulp\n para remover espaços desnesserários.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const sass = require('gulp-sass')(require('sass'));
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
-        .pipe(sass())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
         .pipe(gulp.dest('./build/styles'));
 }
 
