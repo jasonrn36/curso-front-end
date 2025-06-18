102d301c6d87ffe80eb5c7ee38bea353d7fda49c
Jason Ramalho Nascimento <jason.rn36@gmail.com>
2025-06-18 12:25:43 -0400

    criado vários métodos de configuração de compilação do gulp pra SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/styles'));
 }
 
@@ -33,3 +36,7 @@ exports.default = gulp.parallel (funcaoPadrao, digaOi);
 exports.digaOi = digaOi;
 
 exports.sass = compilaSass;
+
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+}
