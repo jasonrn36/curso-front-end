b07ec9e8223caa203cdb7570059108b47b525d7f
Jason Ramalho Nascimento <jason.rn36@gmail.com>
2025-06-18 11:15:27 -0400

    criando tarefas em paralelo no GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const gulp = require('gulp');
 
 function funcaoPadrao(callback) {
-    console.log("\n Executando via \x1b[31mGULP\x1b[37m \n");
+    console.log("\n\x1b[37mExecutando terefas em Paralelo no \x1b[1m \x1b[31mGULP \n\x1b[0m\x1b[33mEssa é a Primeira tarefa\x1b[37m");
     callback();
 }
 
 function digaOi(callback) {
-    console.log("\n\x1b[33m O\x1b[36ml\x1b[33má\x1b[31m \x1b[31mGulp\x1b[34m!!");
+    console.log("\n\x1b[0m\x1b[33m Essa é a segunda tarefa junto com a primeira\x1b[37m");
     digaTchau();
     callback();
 
 }
 function digaTchau(){
-    console.log(" \x1b[33mTchau!!\x1b[1m\x1b[31m Gulp \n \x1b[0m\x1b[37m");
+    console.log("\x1b[0m\x1b[33m Essa é a terceira tarefa junto com a segunda\x1b[37m \n");
 }
 
 exports.default = gulp.parallel (funcaoPadrao, digaOi);
