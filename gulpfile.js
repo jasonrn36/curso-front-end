8101227bd01285798060fbc22fc569afbfa0ff8a
jasonrn36 <jason.rn36@gmail.com>
2025-07-20 11:37:32 -0400

    change name of repository

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,20 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin');
+
+
+function compriressImages(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin)
+    .pipe(gulp.dest('./build/images'));
+}
 
 function comprimeJavaScript() {
     return gulp.src('./source/scripts/*.js')
         .pipe(uglify())
+        .pipe(obfuscate())
         .pipe(gulp.dest('./build/scripts'))
 }
 
@@ -49,3 +59,4 @@ exports.watch = function() {
 }
 
 exports.javascript = comprimeJavaScript;
+exports.images = compriressImages;
