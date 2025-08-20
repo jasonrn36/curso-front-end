729bf2a13df12dcdfdb02ed9f70598cf8fb038d9
jasonrn36 <jason.rn36@gmail.com>
2025-08-19 21:09:34 -0400

    criação do primeiro arquivo padrão grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,12 @@
 module.exports = function(grunt) { 
     grunt.initConfig({
          pkg: grunt.file.readJSON('package.json'),
-         });
+         })
+         grunt.registerTask('olaGrunt', function() {
+            const done = this.async();
+            setTimeout(function(){
+            console.log('Olá Grunt');
+            }, 3000);
+
+         })
 }
