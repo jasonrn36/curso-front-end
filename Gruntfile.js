26c9f6f5d1d7eb84310998f929a8260fa496804c
jasonrn36 <jason.rn36@gmail.com>
2025-08-22 12:14:31 -0400

    Configuração do grunt básico

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
          pkg: grunt.file.readJSON('package.json'),
          })
-         grunt.registerTask('olaGrunt', function() {
-            const done = this.async();
-            setTimeout(function(){
-            console.log('Olá Grunt');
-            }, 3000);
 
+         grunt.registerTask('OlaGrunt', function() {
+            const done = this.async();
+            setTimeout(function() {
+            console.log('Olá Grunt');
+            done();
+            }, 3000);
          })
 
-        grunt.registerTask('default', ['olaGrunt']);
+        grunt.registerTask('default', ['OlaGrunt']);
 
 }
