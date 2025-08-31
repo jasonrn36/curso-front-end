4df0c8b13875ff05bd2d44514f3150cc98462e3a
jasonrn36 <jason.rn36@gmail.com>
2025-08-31 14:11:15 -0400

    consertando e entendento o codigo do replace

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,12 +24,35 @@ module.exports = function(grunt) {
                   files: ['src/styles/**/*.less'], // Aqui os asteriscos indicam que ele ira executar 
                   tasks: ['less:development']
                }
+         },
+
+         replace: {
+               dev: {
+                  options: {
+                     patterns: [
+                        {
+                           match: 'Endereco_Do_CSS',
+                           replacement: './styles/main.css'
+                        }
+                     ]
+                  },
+                  files: [
+                     {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                     }
+                  ]
+               }
          }
+
+
     })
 
       grunt.loadNpmTasks('grunt-contrib-less');
       grunt.loadNpmTasks('grunt-contrib-watch'); 
-
+         grunt.loadNpmTasks('grunt-replace');
 
          grunt.registerTask('default', ['watch']); 
             grunt.registerTask('build', ['less:production']);
---
+++ _README.md
@@ -0,0 +1,5 @@
+# ATENÇÃO AOS PASSOS A SEGUIR #
+
+## NESTE O <span style="color:orange"> *INDEX.HTML* </span> QUE DEVE SER EXECUTADO NO NAVEGADOR É 
+## O QUE ESTÁ DENTRO DA PASTA <span style="color:orange"> *DEV* </span>
+
